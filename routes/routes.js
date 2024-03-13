b98ee6b977c83bbfb038b4abda9342fa9a81d0fc
Fiona Fairlie <f.fairlie@gcu.ac.uk>
2024-03-13 17:59:34 +0000

    minor update

--- routes/routes.js
+++ routes/routes.js
@@ -6,47 +6,60 @@ router.get("/", function (req, res) {
   res.render("indexPage");
 });
 
+// let inputValidator = [
+//     check("password", "Your password must be at least 5 characters")
+//     .not()
+//     .isEmpty()
+//     .isLength({ min: 5 }),
+//   check("classYear", "Class Year should be a number").not().isEmpty().isInt(),
+//   check("weekday", "Choose a weekday")
+//     .optional()
+//     .not()
+//     .isIn(["Sunday", "Saturday"]),
+//   check("email", "Your email is not valid")
+//     .not()
+//     .isEmpty()
+//     .isEmail()
+//     .normalizeEmail(),
+//   check("name")
+//     .exists()
+//     .isLength({ min: 5 })
+//     .trim()
+//     .escape()
+//     .withMessage("Name must have more than 5 characters"),
+//   // check("password", "Passwords do not match").custom(
+//   //   (value, { req }) => value === req.body.password
+//   // ),
+// ];
+
 let inputValidator = [
-    check("password", "Your password must be at least 5 characters")
-    .not()
-    .isEmpty()
-    .isLength({ min: 5 }),
-  check("classYear", "Class Year should be a number").not().isEmpty().isInt(),
-  check("weekday", "Choose a weekday")
-    .optional()
-    .not()
-    .isIn(["Sunday", "Saturday"]),
-  check("email", "Your email is not valid")
-    .not()
-    .isEmpty()
-    .isEmail()
-    .normalizeEmail(),
   check("name")
-    .exists()
-    .isLength({ min: 5 })
-    .trim()
-    .escape()
-    .withMessage("Name must have more than 5 characters"),
-  // check("password", "Passwords do not match").custom(
-  //   (value, { req }) => value === req.body.password
-  // ),
+    .not()
+    .isEmpty()
+    .withMessage("Name should not be empty"),
+  check("classYear", "Class Year should be a number").not().isEmpty(),
+  check("weekday", "Choose a weekday").optional(),
+  check("email", "Your email is not valid").not().isEmpty(),
+  // check('email', 'Your email is not valid').not().isEmpty().isEmail().normalizeEmail(),
+  check("password", "Your password should not be empty")
+    .not()
+    .isEmpty(),
+    // check('name').exists().isLength({min: 5}).trim().escape().withMessage('Name must have more than 5 characters'),
+    // check('confirmPassword', 'Passwords do not match').custom((value, {req}) => (value === req.body.password)),
 ];
 
-router.post(
-  "/",
-  inputValidator,
-  function (req, res) {
-    const result = validationResult(req);
-    const errors = result.array();
-  
-    if (!result.isEmpty()) {
-      res.render("indexPage",{
-        'errors':errors})
-    } else {
-      res.render("nextPage")
-    }
-  }
-);
+router.post("/", inputValidator, function (req, res) {
+  const result = validationResult(req);
+  const errors = result.array();
 
+  console.log(req.body.name)
+  if (!result.isEmpty()) {
+    res.render("indexPage", {
+      errors: errors,
+    });
+  } else {
+    res.render("nextPage");
+  }
+});
 
 module.exports = router;
